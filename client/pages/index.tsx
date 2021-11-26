0d98d025c4d09e772a06703ed62ac0c7103194c3
mythosmystery <axiysfire@gmail.com>
2021-11-26 01:19:00 -0600

    switched to mono repo

---
+++ client/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import { FC } from 'react';
+
+const Home: FC = () => {
+   return (
+      <div className='absolute w-screen min-h-screen py-2 overflow-y-auto overflow-x-hidden scrollbar-hide'>
+         <Head>
+            <title>Friendlie - A Place to Make Friends</title>
+            <link rel='icon' href='/favicon.ico' />
+         </Head>
+         <h1 className='font-rw text-center text-6xl my-8'>Welcome to Friendlie!</h1>
+         <div className='w-full h-full flex justify-center items-center'></div>
+      </div>
+   );
+};
+export default Home;
